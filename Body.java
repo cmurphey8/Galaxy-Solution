018849d0e3558f1f82130c9546c95624cedd38e9
cmurphey8 <53098445+cmurphey8@users.noreply.github.com>
2021-04-12 12:58:59 -0400

    Update Body.java

--- Body.java
+++ Body.java
@@ -1,10 +1,5 @@
 /**********************************************************************************
  *
- *  This is a template file for Wave.java. It lists the constructors and
- *  methods you need, along with descriptions of what they're supposed to do.
- *  
- *  PART 1: complete all methods of the Ball class
- *  PART 2: update Billiards.java so that all balls are represented by the Ball data type
  *              
  **********************************************************************************/
 import java.util.Scanner;
@@ -23,8 +18,7 @@ public class Body {
     
     private double fx;      // x force
     private double fy;      // y force
-
-    private boolean reinit = false;     // reinitialized status
+    
     private boolean removed = false;    // removed status
 
 /**********************************************************************************
